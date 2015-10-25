385ad3c7ebf7b08173108ca9ef039c79e848ef7a
Jameson Toper <james@toperworks.com>
2015-10-24 23:26:06 -0400

    Made VSync stick to 60 frames per second

--- ToperEngine/src/net/toper/VSync.java
+++ ToperEngine/src/net/toper/VSync.java
@@ -4,6 +4,11 @@ public class VSync implements Runnable {
 
 	Frame f;
 
+	long lastTime = System.nanoTime();
+	final double ticks = 60D;
+	double ns = 1000000000 / ticks;
+	double delta = 0;
+
 	public VSync(Frame f) {
 		this.f = f;
 		Thread updateThread = new Thread(this);
@@ -12,7 +17,13 @@ public class VSync implements Runnable {
 
 	public void run() {
 		while (true) {
-			f.endFrame();
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if (delta >= 1) {
+				f.endFrame();
+				delta--;
+			}
 		}
 	}
 }
