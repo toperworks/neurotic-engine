9a3999ac8e5faa6e2527fd108f1660958c5df79d
Jameson Toper <james@toperworks.com>
2015-10-24 23:24:57 -0400

    Made a much smoother rendering method for tiles, even with a ton of tiles, it only renders what needs to be drawn on the screen

--- EngineTest/src/net/toper/graphics/Generate.java
+++ EngineTest/src/net/toper/graphics/Generate.java
@@ -8,11 +8,17 @@ import net.toper.Input;
 public class Generate {
 
 	int size;
-	public static int TILE_SIZE = 1024;
+	public static int TILE_SIZE = 10;
 	Tile[] tiles;
 	Random r = new Random();
 
 	public Generate(int size) {
+		RenderEngine.grass.setScale((float) Generate.TILE_SIZE
+				/ RenderEngine.grass.getWidth());
+		RenderEngine.water.setScale((float) Generate.TILE_SIZE
+				/ RenderEngine.water.getWidth());
+		RenderEngine.nullTex.setScale((float) Generate.TILE_SIZE
+				/ RenderEngine.nullTex.getWidth());
 		this.size = size;
 		tiles = new Tile[size * size];
 		for (int x = 0; x < size; x++) {
@@ -21,9 +27,9 @@ public class Generate {
 				if (rand == 0) {
 					tiles[x + y * size] = new GrassTile(x, y);
 				} else if (rand == 2) {
-					tiles[x + y * size] = new GrassTile(x, y);
+					tiles[x + y * size] = new WaterTile(x, y);
 				} else if (rand == 3) {
-					tiles[x + y * size] = new GrassTile(x, y);
+					tiles[x + y * size] = new WaterTile(x, y);
 				} else {
 					tiles[x + y * size] = new GrassTile(x, y);
 				}
@@ -31,21 +37,27 @@ public class Generate {
 		}
 	}
 
-	float add = 0;
-	boolean subtract;
-
 	public void render(Frame f) {
-		add -= 0.1f * f.getDelta();
 		int xOffset = Input.getScaledMouseX() - (TILE_SIZE * size) / 2;
 		int yOffset = Input.getScaledMouseY() - (TILE_SIZE * size) / 2;
-		for (int y = 0; y < size; y++) {
-			for (int x = 0; x < size; x++) {
-				if ((x * TILE_SIZE + xOffset) > -TILE_SIZE && x * TILE_SIZE + xOffset < f.getScaledWidth()
-						&& y * TILE_SIZE + yOffset > -TILE_SIZE && y * TILE_SIZE + yOffset < f.getScaledHeight())
-					tiles[x + y * size].render(f, -xOffset, -yOffset);
+		int xFarBound = (f.getScaledWidth() / TILE_SIZE)
+				- ((xOffset) / TILE_SIZE);
+		int yFarBound = (f.getScaledHeight() / TILE_SIZE)
+				- ((yOffset) / TILE_SIZE);
+		int yCloseBound = yFarBound - (f.getScaledHeight() / TILE_SIZE);
+		int xCloseBound = xFarBound - (f.getScaledWidth() / TILE_SIZE);
+		for (int y = yCloseBound; y < yFarBound; y++) {
+			for (int x = xCloseBound; x < xFarBound; x++) {
+				if (x >= 0 && y >= 0 && (x * TILE_SIZE + xOffset) > -TILE_SIZE
+						&& x * TILE_SIZE + xOffset < f.getScaledWidth()
+						&& y * TILE_SIZE + yOffset > -TILE_SIZE
+						&& y * TILE_SIZE + yOffset < f.getScaledHeight()) {
+					int index = x + y * size;
+					if (index < tiles.length)
+						tiles[index].render(f, -xOffset, -yOffset);
+				}
 			}
 		}
-		TILE_SIZE = (int) (TILE_SIZE + add);
 	}
 
 }
--- EngineTest/src/net/toper/graphics/RenderEngine.java
+++ EngineTest/src/net/toper/graphics/RenderEngine.java
@@ -8,14 +8,19 @@ public class RenderEngine implements Runnable {
 
 	private Frame f;
 
-	public static Texture textureSheet = ResourceManager.loadTexture("/tiles.png", true);
-	public static Texture grass = ResourceManager.getTexFromSheet(textureSheet, 0, 0, 64, 64);
-	public static Texture water = ResourceManager.getTexFromSheet(textureSheet, 64, 0, 64, 64);
-	public static Texture nullTex = ResourceManager.getTexFromSheet(textureSheet, 128, 0, 64, 64);
+	public static Texture textureSheet = ResourceManager.loadTexture(
+			"/tiles.png", true);
+	public static Texture grass = ResourceManager.getTexFromSheet(textureSheet,
+			0, 0, 64, 64);
+	public static Texture water = ResourceManager.getTexFromSheet(textureSheet,
+			64, 0, 64, 64);
+	public static Texture nullTex = ResourceManager.getTexFromSheet(
+			textureSheet, 128, 0, 64, 64);
 
-	Generate level = new Generate(1000);
+	Generate level;
 
 	public RenderEngine(Frame f) {
+		level = new Generate(5000);
 		this.f = f;
 	}
 
@@ -24,8 +29,6 @@ public class RenderEngine implements Runnable {
 	}
 
 	public void run() {
-		grass.setScale(Generate.TILE_SIZE / grass.getWidth());
-		water.setScale(Generate.TILE_SIZE / water.getWidth());
 		while (true) {
 			f.setTitle("FPS: " + f.getFPS());
 			render();
