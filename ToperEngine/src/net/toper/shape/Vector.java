11e64717e1d0e61e97aadc11ba94c81749934e62
Jameson Toper <james@toperworks.com>
2015-10-30 14:08:30 -0400

    Adding some new capabilities to the Vector class, like multiply and resetting values

--- ToperEngine/src/net/toper/shape/Vector.java
+++ ToperEngine/src/net/toper/shape/Vector.java
@@ -12,4 +12,19 @@ public class Vector {
 		this.x = x;
 		this.y = y;
 	}
+
+	public void updatePosition(float x, float y) {
+		this.y = y;
+		this.x = x;
+	}
+
+	public void multiply(float num) {
+		y *= num;
+		x *= num;
+	}
+
+	public void multiply(float nx, float ny) {
+		y *= ny;
+		x *= nx;
+	}
 }
